6b4267df62834b0f1d53823ff379ac399d855897
Nelson Tontarelli <nelartz01@gmail.com>
2024-01-23 15:31:45 -0300

    Añadiendo nuevo reporte para sale.order

--- logos_invoice/__manifest__.py
+++ logos_invoice/__manifest__.py
@@ -2,26 +2,22 @@
     "name": "Logos_Invoice",
     "version": "1.0",
     "author": "DEVMAN",
-    'category': 'Accounting/Localizations',
+    "category": "Accounting/Localizations",
     "summary": "LATAM Document Types",
-    'description': """
+    "description": """
 Módulo para Logos en presupuestos y facturas de acuerdo al equipo de ventas y el diario de facturación 
 
 """,
-    "depends": [
-        'l10n_latam_invoice_document', 'sale'
-    ],
+    "depends": ["l10n_latam_invoice_document", "sale"],
     "data": [
-        'views/account_journal_view.xml',
-        'views/crm_team_views.xml',
-        'report/report_invoice.xml',
-        'report/sale_report_templates.xml',
-        #'report/sale_report_templates2.xml',
-        'report/sale_report.xml',
-        'report/2/report_invoice_fe.xml',
-        'report/2/report_inherit_sale_order_document.xml',
-        #'security/ir.model.access.csv',
+        "report/report.xml",
+        "report/report_sale_order.xml", 
+        "report/2/report_invoice_fe.xml",
+        "report/2/report_inherit_sale_order_document.xml",
+
+        "views/account_journal_view.xml",
+        "views/crm_team_views.xml",
     ],
-    'installable': True,
-    'license': 'LGPL-3',
+    "installable": True,
+    "license": "LGPL-3",
 }
