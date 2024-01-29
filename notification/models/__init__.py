f96e9f51837c3f0eb3af93ff39fd879d11f87a95
Nelson Tontarelli <nelartz01@gmail.com>
2024-01-29 15:41:29 -0300

    Mejoras en el reporte de ventas y dandole acceso al modelo de ir_config_parameter

--- notification/models/__init__.py
+++ notification/models/__init__.py
@@ -1,1 +1,2 @@
 from . import notification_model
+from . import ir_config_parameter
---
+++ notification/models/ir_config_parameter.py
@@ -0,0 +1,5 @@
+from odoo import models
+
+
+class IrConfigParameterInherit(models.Model):
+    _inherit = "ir.config_parameter"
